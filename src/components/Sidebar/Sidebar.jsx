43b52656ecfd9236f1c9bb026ec26b099d762e7a
jakesummer <jakersummer@gmail.com>
2026-05-28 14:31:37 -0400

    feat: move forms into panels

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Panel from "../Panel/Panel.jsx";
 import PersonalInfoForm from "./PersonalInfoForm/PersonalInfoForm.jsx";
 
 export default function Sidebar({
@@ -7,12 +9,52 @@ export default function Sidebar({
   onItemChange,
   onDeleteItem,
 }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const panels = [
+    {
+      index: 0,
+      title: "Personal Info",
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>account</title>
+          <path d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
+        </svg>
+      ),
+      children: (
+        <PersonalInfoForm
+          data={data.personalInfo}
+          onChange={onPersonalInfoChange}
+        />
+      ),
+    },
+  ];
+
+  function togglePanel(index) {
+    if (activeIndex === index) {
+      setActiveIndex(-1);
+    } else {
+      setActiveIndex(index);
+    }
+  }
+
   return (
-    <>
-      <PersonalInfoForm
-        data={data.personalInfo}
-        onChange={onPersonalInfoChange}
-      />
-    </>
+    <section>
+      {panels.map((panel) => {
+        const i = panel.index;
+        return (
+          <Panel
+            key={i}
+            isActive={activeIndex === i}
+            onClick={() => togglePanel(i)}
+            title={panel.title}
+            icon={panel.icon}
+            headingLevel={1}
+          >
+            {panel.children}
+          </Panel>
+        );
+      })}
+    </section>
   );
 }
