a0177322de5197500b9ea68ac982294b90bc4ed1
jakesummer <jakersummer@gmail.com>
2026-05-28 17:58:06 -0400

    refactor: move Education form item to PreviewCard
    Component that was rendering each of the different educations listed under the education panel which allowed the user to remove/edit them was moved outside of EducationPanel into its own component so it can be used in other panels.

--- src/components/Sidebar/EducationPanel/EducationPanel.jsx
+++ src/components/Sidebar/EducationPanel/EducationPanel.jsx
@@ -1,26 +1,7 @@
 import { useState } from "react";
 import EducationForm from "./EducationForm/EducationForm.jsx";
 import SaveButton from "../../SaveButton/SaveButton.jsx";
-
-function Education({ school, onEdit, onDelete }) {
-  return (
-    <div className="education-sidebar">
-      <p>{school}</p>
-      <button className="edit-education-btn" onClick={onEdit}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <title>Edit</title>
-          <path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
-        </svg>
-      </button>
-      <button className="delete-education-btn" onClick={onDelete}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <title>Delete</title>
-          <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
-        </svg>
-      </button>
-    </div>
-  );
-}
+import PreviewCard from "../../PreviewCard/PreviewCard.jsx";
 
 const initialEducationData = {
   school: "",
@@ -51,9 +32,9 @@ export default function EducationPanel({
           .filter((d) => d.school)
           .map((d) => {
             return (
-              <Education
+              <PreviewCard
                 key={d.id}
-                school={d.school}
+                title={d.school}
                 onDelete={() => onDeleteItem("education", d.id)}
                 onEdit={() => setActiveId(d.id)}
               />
