132e3b17fd4a8a2fbc160764e905e5557b7f6d48
jakesummer <jakersummer@gmail.com>
2026-05-28 13:27:17 -0400

    feat: render personal info form

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,18 @@
+import PersonalInfoForm from "./PersonalInfoForm/PersonalInfoForm.jsx";
+
 export default function Sidebar({
   data,
   onPersonalInfoChange,
   onAddItem,
   onItemChange,
   onDeleteItem,
-}) {}
+}) {
+  return (
+    <>
+      <PersonalInfoForm
+        data={data.personalInfo}
+        onChange={onPersonalInfoChange}
+      />
+    </>
+  );
+}
