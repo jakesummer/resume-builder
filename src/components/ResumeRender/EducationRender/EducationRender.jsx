62b0fff8a5292f7afe09d8bb328d44c7f01781dd
jakesummer <jakersummer@gmail.com>
2026-05-28 19:54:44 -0400

    refactor: change class name of education renders

--- src/components/ResumeRender/EducationRender/EducationRender.jsx
+++ src/components/ResumeRender/EducationRender/EducationRender.jsx
@@ -1,6 +1,6 @@
 export default function EducationRender({ data }) {
   return data.map((d) => (
-    <div key={d.id} className="education-resume">
+    <div key={d.id} className="education-resume-item">
       <div>
         <h3 className="resume-item-heading">{d.school}</h3>
         <p className="education-degree">{d.degree}</p>
